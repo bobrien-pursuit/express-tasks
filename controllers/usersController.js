b98fade374d11c81dcc832565e5671b1b96435c5
Brendan O'Brien <bobrien@pursuit.org>
2024-08-09 12:44:13 -0400

    imports logInUser and creates login route

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const jwt = require('jsonwebtoken')
 // Secret string from .env used when function to create a token is called
 const secret = process.env.SECRET;
-const { getUsers, createUser } = require('../queries/users');
+const { getUsers, createUser, logInUser } = require('../queries/users');
 
 
 users.get('/', async (req, res) => {
@@ -34,6 +34,28 @@ users.post('/', async (req, res) => {
     }
 });
 
+users.post('/login', async (req, res) => {
+    try {
+        const user = await logInUser(req.body);
+        if(!user){
+            res.status(401).json({ error: "Invalid username or password" })
+            return // Exit the function
+        }
+
+        const token = jwt.sign({ userId: user.user_id, username: user.username }, secret);
+
+        res.status(200).json({ 
+            user: {
+                user_id: user.user_id, 
+                username: user.username, 
+                email: user.email
+            }, 
+            token 
+        });
+    } catch (err) {
+        res.status(500).json({ error: "Internal server error" })
+    }
+})
 
 
 
