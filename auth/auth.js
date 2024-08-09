481e23ede0f39e828c8c4b2b303049859314242f
Brendan O'Brien <bobrien@pursuit.org>
2024-08-09 18:40:36 -0400

    removes curly braces

--- auth/auth.js
+++ auth/auth.js
@@ -4,12 +4,14 @@ const secret = process.env.SECRET;
 
 const authenticateToken = (req, res, next) => {
     const token = req.header('Authorization')
-    if(!token){
+    if(!token)
         return res.status(401).json({ "error": "Access Denied. No token provided" })
-    }
+    
     jwt.verify(token, secret, (err, user) => {
         if (err) return res.status(403).json({ error: 'Invalid token.' });
+        
         req.user = user;
+        
         next();
     })
 }
